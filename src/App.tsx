bf42a1136b85cbb055d664ef4e7a6000f41edbe3
seyoung97 <seyoung1st@gmail.com>
2023-12-06 15:59:35 +0900

    Fix: tailwind config content 설정 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import RevenueCalendar from "./pages/RevenueCalendar";
 import ProductAnalysis from "./pages/ProductAnalysis";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<RevenueCalendar />} />
         <Route path="/product_analysis" element={<ProductAnalysis />} />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,9 @@
+import logo from "../assets/images/logo/paylog_logo.png";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="w-full border-solid border-b-2 border-slate-400 bg-inherit">
+      <img src={logo} className="w-100px"></img>
+    </div>
+  );
 }
