5d249f1e5689fbe460c0584b5ced7ec7f6780986
seyoung97 <seyoung1st@gmail.com>
2023-12-08 16:09:28 +0900

    Feat KAN-16: calendar Header UI

--- src/pages/RevenueCalendar.tsx
+++ src/pages/RevenueCalendar.tsx
@@ -1,3 +1,41 @@
+import { useState } from "react";
+import {
+  startOfMonth,
+  endOfMonth,
+  startOfWeek,
+  endOfWeek,
+  eachDayOfInterval,
+  format,
+  addMonths,
+  subMonths,
+} from "date-fns";
+import koLocale from "date-fns/locale/ko";
+
+import CalendarHeader from "../components/CalendarHeader";
+
 export default function RevenueCalendar() {
-  return <div>매출 달력 페이지</div>;
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+
+  const getMonthDays = () => {
+    const startDate = startOfWeek(startOfMonth(currentMonth));
+    const endDate = endOfWeek(endOfMonth(currentMonth));
+    return eachDayOfInterval({ start: startDate, end: endDate });
+  };
+
+  const handlePrevMonth = () => {
+    setCurrentMonth(subMonths(currentMonth, 1));
+  };
+
+  const handleNextMonth = () => {
+    setCurrentMonth(addMonths(currentMonth, 1));
+  };
+  return (
+    <div>
+      <CalendarHeader
+        headerData={format(currentMonth, "MMMM yyy", { locale: koLocale })}
+        onPrevMonthClick={handlePrevMonth}
+        onNextMonthClick={handleNextMonth}
+      />
+    </div>
+  );
 }
