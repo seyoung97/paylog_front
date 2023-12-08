4825c521a1f4013eecbc9d37e6c9aa5b8998be5a
Joey Chung <108709932+seyoung97@users.noreply.github.com>
2023-12-08 14:44:27 +0900

    Kan 7 layout (#1)
    - Header UI
    - Menu UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
+import Menu from "./Menu";
 import logo from "../assets/images/logo/paylog_logo.png";
+import { IoMenu } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   return (
-    <div className="w-full border-solid border-b-2 border-slate-400 bg-inherit">
-      <img src={logo} className="w-100px"></img>
-    </div>
+    <>
+      <div className="flex justify-between items-center w-full border-b border-gray-200 shadow-sm">
+        <div className="w-1/12 mx-4 py-3">
+          <img src={logo} className="w-full"></img>
+        </div>
+        {isMenuOpen ? (
+          <IoClose size="2rem" className="mr-4" onClick={handleMenu} />
+        ) : (
+          <IoMenu size="2rem" className="mr-4" onClick={handleMenu} />
+        )}
+      </div>
+      <Menu isMenuOpen={isMenuOpen} />
+    </>
   );
 }
