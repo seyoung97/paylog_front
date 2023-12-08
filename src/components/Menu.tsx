273250e14a90c7658dbe0c0cb1996ba9be3325f8
seyoung97 <seyoung1st@gmail.com>
2023-12-08 11:40:54 +0900

    🎨UI: Header, Menu UI

---
+++ src/components/Menu.tsx
@@ -0,0 +1,41 @@
+import { useNavigate } from "react-router";
+
+type Props = {
+  isMenuOpen: boolean;
+};
+
+export default function Menu({ isMenuOpen }: Props) {
+  console.log("menu", isMenuOpen);
+  const navigate = useNavigate();
+
+  const navigateToCalendar = () => {
+    navigate("/");
+  };
+
+  const navigateToProductAnalysis = () => {
+    navigate("/product_analysis");
+  };
+
+  const menuAnimationStyle = isMenuOpen ? "translate-x-0" : "translate-x-full";
+
+  return (
+    <div
+      className={`w-1/4 h-screen fixed ${menuAnimationStyle} border-l border-gray-300`}
+    >
+      <ul className="pt-8 pl-5">
+        <li
+          className="pb-5 text-lg cursor-pointer"
+          onClick={navigateToCalendar}
+        >
+          매출 달력
+        </li>
+        <li
+          className="text-lg cursor-pointer"
+          onClick={navigateToProductAnalysis}
+        >
+          상품 분석
+        </li>
+      </ul>
+    </div>
+  );
+}
