9a48cdeabfdfb8fab072e1818c189a391ef96e54
seyoung97 <seyoung1st@gmail.com>
2023-12-20 11:26:01 +0900

    Design KAN-45:로그인 모달UI

--- src/components/OrderCategoryTab.tsx
+++ src/components/OrderCategoryTab.tsx
@@ -1,11 +1,25 @@
 import { useState } from "react";
 
-export default function OrderCategoryTab() {
+type Props = {
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function OrderCategoryTab({ setIsModalOpen }: Props) {
   const [category, setCategory] = useState("인기 메뉴");
-  const [isAdmin, setIsAdmin] = useState(true);
+  const [count, setCount] = useState(0);
+  // const [isAdmin, setIsAdmin] = useState(true);
   const categories = ["인기 메뉴", "점심", "저녁", "사이드", "주류"];
   const focusedStyle = "border-b-4 border-sky-500 text-sky-500";
   const unfocusedStyle = "text-slate-600";
+
+  const handleLoginModalOpen = () => {
+    setCount((prev) => prev + 1);
+    console.log("count", count);
+    if (count === 7) {
+      setIsModalOpen(true);
+      setCount(0);
+    }
+  };
 
   return (
     <div className="flex mx-5 w-9/12">
@@ -24,7 +38,11 @@ export default function OrderCategoryTab() {
           </div>
         );
       })}
-      {isAdmin && <div className="px-9 py-3 font-semibold">+ 카테고리</div>}
+      {/* {isAdmin && <div className="px-9 py-3 font-semibold">+ 카테고리</div>} */}
+      <div
+        className="px-12 py-5 bg-sky-200"
+        onClick={handleLoginModalOpen}
+      ></div>
     </div>
   );
 }
--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -1,13 +1,76 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import OrderCategoryTab from "../components/OrderCategoryTab";
 import Cart from "../components/Cart";
 import ProductMenu from "../components/ProductMenu";
+import logo from "../assets/images/logo/paylog_logo.png";
+
+const modalCustomStyles = {
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.7)",
+  },
+  content: {
+    width: "40%",
+    height: "60%",
+    margin: "auto",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    border: "2px solid rgb(14 165 233)",
+    borderRadius: "0.8rem",
+  },
+};
+Modal.setAppElement("#root");
 
 export default function Order() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  console.log("isModalOpen", isModalOpen);
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div>
-      <OrderCategoryTab />
+      <OrderCategoryTab setIsModalOpen={setIsModalOpen} />
       <Cart />
       <ProductMenu />
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={() => setIsModalOpen(false)}
+        style={modalCustomStyles}
+      >
+        <div className="w-1/5 mt-5">
+          <img src={logo} />
+        </div>
+        <form className="flex flex-col items-center w-full mt-20">
+          <div className="flex justify-center items-center w-full mb-7 ">
+            <label htmlFor="id" className="text-lg font-bold">
+              아이디{" "}
+            </label>
+            <input
+              type="text"
+              id="id"
+              name="id"
+              className="w-7/12 ml-6 py-1 px-1 bg-gray-100 border border-slate-500 rounded-md"
+            />
+          </div>
+          <div className="flex justify-center items-center w-full">
+            <label htmlFor="password" className="text-lg font-bold">
+              비밀번호{" "}
+            </label>
+            <input
+              type="text"
+              id="password"
+              name="password"
+              className="w-7/12 ml-3 py-1 px-1 bg-gray-100 border border-slate-500 rounded-md"
+            />
+          </div>
+          <button className="w-5/12 mt-20 py-2 bg-sky-500 text-white font-bold rounded-md">
+            로그인
+          </button>
+        </form>
+      </Modal>
     </div>
   );
 }
